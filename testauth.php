c62bf66d30d37c142089e835eec804c22f9d4b3a
alhafram <alhafram@gmail.com>
2016-07-20 21:00:56 +0300

    Add files via upload

---
+++ testauth.php
@@ -0,0 +1,13 @@
+
+<form method="POST" enctype="multipart/form-data" action="users/registrationUser.php">
+
+    <input type="hidden" name="UserLogin" value="qwerty1111" />
+    
+    <input type="hidden" name="UserPassword" value="qwerty1111" />
+    
+    <input type="hidden" name="UserEmail" value="alhafram@gmail.com" />
+    
+    
+    <input type="submit" name="submit" />
+
+</form>
